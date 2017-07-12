cccc8d3f77325ab5c890b25a82a6a1dffcf1d6ea
yektas <ahmetsercanyektas@gmail.com>
2017-07-12 14:51:00 +0300

    bug fix

--- users/views.py
+++ users/views.py
@@ -5,12 +5,12 @@ from .models import EventClass
 ####AUTHENTICATION METHODS
 
 def login(request):
-    c = {}
-    c.update(csrf(request))
-    return render(request, 'login.html', c)
+    if request.session['is_logged'] == True:
+        c = {}
+        c.update(csrf(request))
+        return render(request, 'login.html', c)
 
 def auth_login(request):
-
     username = request.POST.get('username')
     password = request.POST.get('password')
     user_info = request.POST
@@ -23,6 +23,7 @@ def auth_login(request):
         request.session['password'] = password
         request.session['is_logged'] = True
         return redirect("users:index")
+
     else:
         return redirect("users:login")
 
@@ -30,6 +31,16 @@ def index(request):
     return render(request, "index.html")
 
 def register(request):
+    name = request.POST.get('name')
+    surname = request.POST.get('surname')
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    user_info = request.POST
+
+    instance = EventClass(user_info)
+    user = instance.register_event()
+
     return render(request, "register.html")
 
 def profile(request):
@@ -38,4 +49,10 @@ def profile(request):
                     }
     return render(request, "profile.html", user_content)
 
+def adminview(request):
+    return render(request, "admin.html")
+def oyunekle(request):
+    return render(request, "ekle.html")
+def oyunsil(request):
+    return render(request, "oyunsil.html")
 
