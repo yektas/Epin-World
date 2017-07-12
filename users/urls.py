9a8e04919177e0bfa6c6b0d93cf4f5fee92005fa
utkucanbykl <utkucanbykl@gmail.com>
2017-07-12 14:42:39 +0300

    Urls

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^$', views.index, name='index'),
     url(r'^auth_login/$', views.auth_login, name='auth_login'),
+    url(r'^adminn/$', views.adminview, name='adminview'),
+    url(r'^oyunekle/$', views.oyunekle, name='oyunekle'),
+    url(r'^oyunsil/$', views.oyunsil, name='oyunsil'),
+    url(r'^userslist/$', views.userslist, name='userslist'),
+
     #url(r'^loggedin/$', views.loggedin, name='loggedin'),
 ]
