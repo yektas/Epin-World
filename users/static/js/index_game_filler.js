9722d27d20d506c78cb33f2cf6e4e2b107c8474e
admanage <asddeneme3@gmail.com>
2017-07-14 02:28:02 +0300

    Js'ler eklenerek index page üzerinde eklemeler yapıldı.

---
+++ users/static/js/index_game_filler.js
@@ -0,0 +1,19 @@
+var game_table_html = " "
+           $.ajax({
+
+                type: 'GET',
+                url: 'http://localhost:8000/games/games_json/',
+                data: { get_param: 'value' },
+                    dataType: 'json',
+                    success: function (data) {
+                            var json = data;
+
+
+                    $.each(json,function(i,item){
+                         game_table_html += "<div class='col-md-4 chain-grid'>" +"<a href='single.html'><img class='img-responsive chain' src="+ json[i].game_image_url +"alt=' ' /></a>" + "<span class='star'> </span> <div class='grid-chain-bottom'> <h6><a href='single.html'> " +json[i].game_name+"</a></h6>" +  "<div class='star-price'> <div class='dolor-grid'>" +"<span class='actual'>"+json[i].game_money_price+"</span>" +"	</div>"+"</div>" +"<div class='clearfix'> </div> </div> </div> </div>"
+
+                     });
+                     $('#game_index_table').append(game_table_html)
+
+                }
+});
