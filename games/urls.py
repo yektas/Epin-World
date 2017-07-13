222f1b66b700666fba3a2b0b1514aa34b914d9bd
tahademir95 <tahadmr.1995@gmail.com>
2017-07-13 11:14:26 +0300

    Arkadan alınan json dinamik olarak ekrana basılıyor

--- games/urls.py
+++ games/urls.py
@@ -6,4 +6,6 @@ app_name = 'games'
 urlpatterns = [
     #url(r'^(?P<game_name>[\w-]+)/$', views.games,name= 'games'),
     url(r'^detail/$', views.detail,name= 'detail'),
+    url(r'^games_json/$', views.games_json, name='games_json'),
+
 ]
