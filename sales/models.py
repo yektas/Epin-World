cdb2fad989b753e96c5241ef82ade09c576ce391
utkucanbykl <utkucanbykl@gmail.com>
2017-07-15 23:22:30 +0300

    Add Basic Sale Method

--- sales/models.py
+++ sales/models.py
@@ -1,1 +1,24 @@
 from django.db import connection
+
+
+class SaleModel():
+
+    def __init__(self):
+        self.cursor = connection.cursor()
+
+
+    def CreateSaleRow(self, username, gameName, count):
+        """Find User and Game id and Insert Sale Table"""
+        self.cursor.execute("""select id from game where name = '{}'
+        """.format(str(gameName)))
+        gameId = self.cursor.fetchone()[0]
+        self.cursor.execute("SELECT id FROM users WHERE username = '{}' ".format(str(username)))
+        userId = self.cursor.fetchone()[0]
+        self.cursor.execute("select price from game WHERE name = '{}' ".format(str(gameName)))
+        amount = self.cursor.fetchone()[0] * count
+        try:
+            self.cursor.execute("insert into sale (game_id,user_id,amount) VALUES ('{}','{}',{}) ;".format(gameId,userId,amount))
+            return True
+        except:
+            return False
+            """Add Log"""
--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from sales.models import SaleModel
+from users.models import EventClass
+
+saleModel = SaleModel()
+
 def checkout(request):
-    return render(request, "checkout.html")
+    instance = EventClass(request)
+    games = instance.list_games()
+    return render(request, "sale.html", {"games":games})
+
+
+def AddCartView(request):
+    """Add Cart Function """
+    if request.method == "POST":
+        try:
+            game_name = request.POST['game']
+            hm = request.POST['hm']
+        except:
+            hm = -1
+            game_name = "Diablo1"
+    user_name = "sercan"
+
+    saleModel.CreateSaleRow(user_name, game_name, int(hm))
+    request.session["game"] = game_name
+    return render(request, "sale.html")
