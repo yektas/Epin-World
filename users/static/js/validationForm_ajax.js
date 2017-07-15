74d8cea81d12d7ad8150cc9f79ae36cf2899fdc9
tahademir95 <tahadmr.1995@gmail.com>
2017-07-15 14:01:41 +0300

    javascript function in the base.html removed to the js file

---
+++ users/static/js/validationForm_ajax.js
@@ -0,0 +1,34 @@
+/**
+ * Created by taha on 14.07.2017.
+ */
+var game_table_html = " "
+                    $.ajax({
+                        type: 'GET',
+                        url: 'http://localhost:8000/games/games_json/',
+                        data: { get_param: 'value' },
+                        dataType: 'json',
+                        success: function (data) {
+                                var json = data;
+
+                        $.each(json,function (i,item){
+                            game_table_html +=  "<li class='subitem1'><a href= " + json[i].game_name.toLowerCase() + ".html>" + json[i].game_name +"</a></li>"
+
+                        });
+                        $('#oyunlar').append(game_table_html)
+                }
+        });
+        // allows users to sign up to system if  they filled the necessary field.
+        function validateForm(){
+		    var name = document.forms["regForm"]["name"].value;
+		    var surname = document.forms["regForm"]["surname"].value;
+		    var username = document.forms["regForm"]["username"].value;
+		    var email = document.forms["regForm"]["email"].value;
+		    var password = document.forms["regForm"]["password"].value;
+
+		    var re = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+
+		    if (username == "" && surname == "" && username == "" && email == "" && password == "" && re.test(password)){
+		        alert("All of the fields must be filled and password should be Minimum eight characters, at least one letter and one number !!");
+		        return false;
+			}
+		}
