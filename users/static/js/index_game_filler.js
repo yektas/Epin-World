d43a6f6008a9cf0a95bc01746042ef549be0e1e0
admanage <asddeneme3@gmail.com>
2017-07-15 23:10:27 +0300

    Dinamik link oluşturmadaki sorun handle edildi

--- users/static/js/index_game_filler.js
+++ users/static/js/index_game_filler.js
@@ -10,7 +10,7 @@ var game_table_html = " "
 
 
                     $.each(json,function(i,item){
-                         game_table_html += "<div class='col-md-4 chain-grid'>" +"<a href='single.html'><img class='img-responsive chain' src="+ json[i].game_image_url +"alt=' ' /></a>" + "<span class='star'> </span> <div class='grid-chain-bottom'> <h6><a href='single.html'> " +json[i].game_name+"</a></h6>" +  "<div class='star-price'> <div class='dolor-grid'>" +"<span class='actual'>"+json[i].game_money_price+"</span>" +"	</div>"+"</div>" +"<div class='clearfix'> </div> </div> </div> </div>"
+                         game_table_html += "<div class='col-md-4 chain-grid'>" +"<a href='/users/game_detail/"+json[i].game_name+ "'/'"+">"+"<img class='img-responsive chain' src="+ json[i].game_image_url +"alt=' ' /></a>" + "<span class='star'> </span> <div class='grid-chain-bottom'> <h6><a href= "+ "/users/game_detail/Diablo3/" +json[i].game_name+"></a>"+ json[i].game_name +"</h6>" +  "<div class='star-price'> <div class='dolor-grid'>" +"<span class='actual'>"+json[i].game_money_price+"</span>" +"	</div>"+"</div>" +"<div class='clearfix'> </div> </div> </div> </div>"
 
                      });
                      $('#game_index_table').append(game_table_html)
