dc9f39d3b0b9390940cdc3abc26dc0e4e346f3f1
admanage <asddeneme3@gmail.com>
2017-07-17 15:32:07 +0300

    Javascriptlerdeki buglar handle edildi formvalidationdaki validation hataları düzelti

--- users/static/js/validationForm_ajax.js
+++ users/static/js/validationForm_ajax.js
@@ -2,30 +2,7 @@
  * Created by taha on 14.07.2017.
  */
 //Adds a new game-which is added by the admin-to the navigation bar in the homepage
-var game_table_html = " "
-var k = 0;
-                    $.ajax({
-                        type: 'GET',
-                        url: 'http://localhost:8000/games/games_json/',
-                        data: { get_param: 'value' },
-                        dataType: 'json',
-                        success: function (data) {
-                                var json = data;
 
-                        $.each(json,function (i,item){
-                            game_table_html +=  "<li class='subitem1'><a href= " + json[i].game_name.toLowerCase() + ".html>" + json[i].game_name +"</a></li>"
-
-                            k += 1
-                            if(k==2){
-                                
-                            }
-
-                        });
-
-                        console.log("asdasd");
-                        $('#oyunlar').append(game_table_html)
-                }
-        });
         // allows users to sign up to system if  they filled the necessary field.
         function validateForm(){
 		    var name = document.forms["regForm"]["name"].value;
