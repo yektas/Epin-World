b892a133df08f1a13836b9fc6cf106a64ad4cc2c
admanage <asddeneme3@gmail.com>
2017-07-18 12:54:18 +0300

    Ön yüzdeki

--- users/static/js/index_game_filler.js
+++ users/static/js/index_game_filler.js
@@ -1,4 +1,5 @@
 var game_table_html = " "
+alert("asd");
 var game_table_html_dropdown_list = " "
            $.ajax({
 
@@ -11,7 +12,7 @@ var game_table_html_dropdown_list = " "
 
 
                     $.each(json,function(i,item){
-                        if(i<=3){
+                        if(i<=5){
                             game_table_html += "<div class='col-md-4 chain-grid'>" +"<a href='/users/game_detail/"+json[i].game_name+ "'/'"+">"+"<img class='img-responsive chain' src="+ json[i].game_image_url +"alt=' ' /></a>" + "<span class='star'> </span> <div class='grid-chain-bottom'> <h6><a href= "+ "/users/game_detail/Diablo3/" +json[i].game_name+"></a>"+ json[i].game_name +"</h6>" +  "<div class='star-price'> <div class='dolor-grid'>" +"<span class='actual'>"+json[i].game_money_price+"</span>" +"    </div>"+"</div>" +"<div class='clearfix'> </div> </div> </div> </div>"
                             game_table_html_dropdown_list +=  "<li class='subitem1'><a href= " + json[i].game_name.toLowerCase() + ".html>" + json[i].game_name +"</a></li>"
 
--- users/static/js/index_page_products.js
+++ users/static/js/index_page_products.js
@@ -1,4 +1,43 @@
-/*Tooltip*/
+
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
+alert("asd")
+
+var game_product_footer = ""
+
+$.ajax({
+
+                type: 'GET',
+                url: 'http://localhost:8000/games/games_json/',
+                data: { get_param: 'value' },
+                    dataType: 'json',
+                    success: function (data) {
+                            var json = data;
+
+
+                    $.each(json,function(i,item){
+                        if(i<=3){
+                          /*
+                            game_product_footer +=  "<div class='col-sm-3'> <article class='col-item'> <div class='photo'> <div class='options-cart'>" +" <a href='/users/game_detail/" +json[i].game_name+"> <button class='btn btn-default' title='Add to cart'> <span class='fa fa-shopping-cart'></span> </button> </a></div>"
+                            game_product_footer += "<a href=''> <img src='{% static 'images/json[i].game_name+.jpg'%}' class='img-responsive' alt='Product Image' /> </a>"
+                            game_product_footer += "</div><div class='container'><br> <strong><p>"+json[i].game_name+"</p></strong>"
+                            game_product_footer += " <strong><p>"+json[i].game_money_price+"</p></strong>"
+                            game_product_footer += "  </div></article></div>"
+                          */
+                        }
+
+                     });
+                     $('#products_footer').append(game_product_footer)
+
+                }
+});
+
+
+
+
+
+
+
+
