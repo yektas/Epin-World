f778b89ce2037b16e7fa1a05695c7f775197ad37
utkucanbykl <utkucanbykl@gmail.com>
2017-07-21 11:53:57 +0300

    Pattern bug fix

--- games/urls.py
+++ games/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^games_json/$', views.games_json, name='games_json'),
     url(r'^game_detail_search/$', views.generate_detail_html, name="detail_search"),
     url(r'^game_detail/(?P<game_name>\w+)$', views.generate_detail_html),
+    url(r'^search/$', views.SearchView, name="search_game"),
 
 ]
--- home/tests.py
+++ home/tests.py
@@ -1,1 +1,30 @@
 # Create your tests here.
+import unittest
+
+from django.test import Client
+
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def AdminPage(self):
+        """Admin Page 200 Donduruyor mu"""
+        response = self.client.get("/adminn/")
+        self.assertEqual(response.status_code, 200)
+
+    def PageNotFound(self):
+        """Random bir sayfa 404 veriyor mu"""
+        response = self.client.get("/adasdada/")
+        self.assertEqual(response.status_code, 404)
+
+    def Login(self):
+        """Post degerleri verilen login 200 veriyor mu"""
+        self.client.get("/users/login/", {'username': 'sercan', 'password': '12345'})
+        session = self.client.session
+        print(session["id"])
+        self.assertEqual(session['is_logged'], True)
+
+    def SqlInjection(self):
+        response = self.client.get("/users/login/", {"username": "' or True --", "password": "asdada"})
+        self.assertEqual(response.context['is_logged'], True)
