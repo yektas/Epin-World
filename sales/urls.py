ea4d461e4804aa09d4590736b05af217f791436b
jrlinq <ahmetsercanyektas@gmail.com>
2017-07-25 13:24:59 +0300

    Add to cart functionality done

--- sales/urls.py
+++ sales/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-
 from sales import views
 
 app_name = 'sales'
@@ -7,4 +6,5 @@ app_name = 'sales'
 urlpatterns = [
     url(r'^checkout/$', views.checkout, name='checkout'),
     url(r'^addtocart/$', views.addtocart, name='addtocart'),
+    url(r'^cart_json/$', views.cart_json, name='cart_json'),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 import json
+from games.models import GameEventClass
 
 def checkout(request):
     product_list = {}
@@ -14,22 +15,40 @@ def checkout(request):
 
 
 def addtocart(request):
-    quantity = 0
 
-    game_name = request.POST.get('game_name', ' ')
     game_id = request.POST.get('game_id', ' ')
-    game_price = request.POST.get('game_price', ' ')
-
-    game_data = {'game_name': game_name, 'game_id': game_id, 'game_price': game_price, 'quantity': quantity}
 
     if 'cart' not in request.session or request.session['cart'] is None:
-        request.session['cart'] = game_data
-        if game_id == request.session['cart']['game_id']:
-            request.session['cart']['quantity'] += 1
-        elif game_id != request.session['cart']['game_id']:
-            temp_list = []
-            temp_list = request.session['cart']
+        quantity = 1
+        request.session['cart'] = [{'game_id': game_id, 'quantity': quantity}]
+
+    else:
+        found = False
+        for item in request.session['cart']:
+            if game_id == item['game_id']:
+                qTemp = int(item['quantity'])
+                qTemp += 1
+                item['quantity'] = qTemp
+                found = True
+                break
+        if not found:
+            game_list = []
+            game_list.append({'game_id': game_id, 'quantity': 1})
+            game_list.extend(request.session['cart'])
             del request.session['cart']
-            temp_list.append(game_data)
-            request.session['cart'] = temp_list
+            request.session['cart'] = game_list
+
     return HttpResponse(json.dumps(request.session['cart']))
+
+
+def cart_json(request):
+    instance = GameEventClass()
+    if 'cart' in request.session:
+        cart = []
+        for item in request.session['cart']:
+            game = instance.get_game_byid(int(item['game_id']))
+            cart.append({'game_name': '{}'.format(game[0]['name']), 'quantity': '{}'.format(item['quantity']),
+                         'logo': '{}'.format(game[0]['logo']), 'price': '{}'.format(game[0]['price'])})
+        return HttpResponse(json.dumps(cart), content_type='application/json')
+    else:
+        return HttpResponse("Cart is Empty")
