6caf2a7d37e890c83fd903e27c455406b5b05622
jrlinq <ahmetsercanyektas@gmail.com>
2017-08-02 19:07:24 +0300

    Sepet toplamı ve checkout sayfasındaki adet arttırımları tamamlandı.Bir ürün eklendiğinde yada silindiğinde ajax call ları ile session datası güncelleniyor.

--- sales/urls.py
+++ sales/urls.py
@@ -7,6 +7,7 @@ app_name = 'sales'
 urlpatterns = [
     url(r'^checkout/$', views.checkout, name='checkout'),
     url(r'^addtocart/$', views.addtocart, name='addtocart'),
+    url(r'^update_cart/$', views.update_cart, name='update_cart'),
     url(r'^deletecartitem/$', views.deletecartitem, name='deletecartitem'),
     url(r'^cart_json/$', views.cart_json, name='cart_json'),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
 
 from games.models import GameEventClass
@@ -8,13 +8,35 @@ from games.models import GameEventClass
 
 def checkout(request):
     product_list = {}
-
+    total = 0
     if 'cart' in request.session:
         product_list = request.session['cart']
+        for item in product_list:
+            quantity = int(item['quantity'])
+            price = float(item['game_price'])
+            subtotal = quantity * price
+            item['subtotal'] = subtotal
+            total += subtotal
+        request.session['cartTotal'] = total
     else:
         return HttpResponse("Your cart is empty")
 
     return render(request, "checkout.html", {'product_list': product_list})
+
+
+def update_cart(request):
+    game_id = request.POST.get('game_id', ' ')
+    newSubtotal = request.POST.get('newSubtotal', ' ')
+    quantity = request.POST.get('quantity', ' ')
+    if not request.is_ajax() or not request.method == 'POST':
+        return HttpResponseNotAllowed(['POST'])
+
+    for item in request.session['cart']:
+        if item['game_id'] == game_id:
+            item['subtotal'] = newSubtotal
+            item['quantity'] = quantity
+            break
+    return HttpResponse('Done')
 
 
 def addtocart(request):
@@ -24,9 +46,11 @@ def addtocart(request):
     logo = request.POST.get('logo', ' ')
 
     if 'cart' not in request.session or request.session['cart'] is None:
+        request.session['cartTotal'] = 0
         quantity = 1
         request.session['cart'] = [{'game_name': game_name, 'game_price': game_price,
                                     'game_id': game_id, 'quantity': quantity, 'logo': logo}]
+        request.session['cartTotal'] = game_price
     else:
         found = False
         for item in request.session['cart']:
@@ -35,6 +59,7 @@ def addtocart(request):
                 qTemp += 1
                 item['quantity'] = qTemp
                 found = True
+                request.session['cartTotal'] = float(request.session['cartTotal']) + float(item['game_price'])
                 break
         if not found:
             game_list = []
@@ -43,7 +68,11 @@ def addtocart(request):
             game_list.extend(request.session['cart'])
             del request.session['cart']
             request.session['cart'] = game_list
-
+            if 'cartTotal' not in request.session:
+                request.session['cartTotal'] = 0
+                request.session['cartTotal'] = float(request.session['cartTotal']) + float(game_price)
+            else:
+                request.session['cartTotal'] = float(request.session['cartTotal']) + float(game_price)
     return HttpResponse(json.dumps(request.session['cart']))
 
 
@@ -60,6 +89,8 @@ def deletecartitem(request):
 
             del request.session['cart']
             request.session['cart'] = game_list
+            totalPrice = float(item['quantity']) * float(item['game_price'])
+            request.session['cartTotal'] = float(request.session['cartTotal']) - float(totalPrice)
 
     return HttpResponse(json.dumps(request.session['cart']))
 
