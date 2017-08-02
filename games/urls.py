be84ce38392c94caadb30e49b52de5833f6e06b1
jrlinq <ahmetsercanyektas@gmail.com>
2017-08-02 14:25:56 +0300

    Logo Eklemedeki sorun giderildi. Oyun isminde boşluk olduğu zaman url generate edilememe hatası düzeltildi.

--- games/urls.py
+++ games/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^games_json/$', views.games_json, name='games_json'),
     url(r'^game_detail_search/$', views.generate_detail_html, name="detail_search"),
     url(r'^platform_json/$', views.platform_json, name="platform_json"),
-    url(r'^game_detail/(?P<game_name>\w+)$', views.generate_detail_html, name="game_detail"),
+    url(r'^game_detail/(?P<game_name>[\w|\W]+)$', views.generate_detail_html, name="game_detail"),
     url(r'^search/$', views.SearchView, name="search_game"),
     url(r'^all/$', views.all, name="all_game"),
 
